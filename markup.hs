81e776f6ecb63ce65a3bfe7e233aeac2098575f8
Peter Seibel <peter@gigamonkeys.com>
2014-07-12 16:04:01 -0700

    Trivial parser.

--- markup.hs
+++ markup.hs
@@ -1,1 +1,18 @@
-main = putStrLn "hello, world!"
+import Text.ParserCombinators.Parsec
+
+document :: GenParser Char st String
+document = do
+  text <- text
+  eof
+  return text
+
+text :: GenParser Char st String
+text = many anyChar
+
+main = do
+  putStrLn $ case testParse "hello, world!" of
+    Left err  -> "Error: " ++ show err
+    Right doc -> "Document: " ++ show doc
+
+testParse :: String -> Either ParseError String
+testParse input = parse document "(unknown)" input
